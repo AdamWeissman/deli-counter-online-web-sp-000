9f22d87e491f36f389fb03ab0f89a1fe7d805842
Adam Weissman <adam@comedytragedyepic.com>
2019-09-14 09:20:54 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,8 +1,8 @@
 # Write your code here.
 
 def line(deli)
-  case
-  when deli.size == 0
+  case deli.length
+  when 0
     puts "The line is currently empty"
   else
     puts "There are #{deli.size} people on line")

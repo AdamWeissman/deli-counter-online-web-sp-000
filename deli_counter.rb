e843ee4324d6cf48ff09a9dabd2ec8dd26aa457b
Adam Weissman <adam@comedytragedyepic.com>
2019-09-14 09:55:56 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -16,5 +16,5 @@ end
 
 def take_a_number(current_line=[], new_customer)
   new_line = current_line << new_customer
-  return puts "Welcome, #{new_customer}. You are currently number #{new_line.index.last} in line."
+  return puts "Welcome, #{new_customer}. You are currently number #{new_line.(index+1).last} in line."
 end

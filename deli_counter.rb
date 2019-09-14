08acd66c250cd377666345aa665d3a6c906d88af
Adam Weissman <adam@comedytragedyepic.com>
2019-09-14 09:42:11 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -2,7 +2,9 @@
 
 def line(deli)
   if deli.length > 0
-    print "The line is currently: #{print deli.each_with_index {|name, index| '#{index}. #{name} '}}"
+    the_line = deli.each_with_index {|name, index| ' #{index}. #{name}'}
+    the_line.unshift("The line is currently:")
+    return the_line.join
   else
     puts "The line is currently empty."
   end

4835d5b89dbd998a00ff946099c9c45dad73432d
Adam Weissman <adam@comedytragedyepic.com>
2019-09-14 09:19:10 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,7 +1,5 @@
 # Write your code here.
 
-katz_deli = []
-
-def line(deli=katz_deli)
+def line(deli)
   return deli.empty? (puts "The line is currently empty") : (puts "There are #{line.size} people on line")
 end

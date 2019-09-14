b2f56f12123b61bd668bce749ec9a1817575516b
Adam Weissman <adam@comedytragedyepic.com>
2019-09-14 09:27:05 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -3,7 +3,7 @@
 def line(deli)
   if deli.length > 0
     deli.with_index { |val,index| 
-    puts "The line is currently: "#{val} for #{val}" }
+    puts "The line is currently: "#{index}. #{val}" }
   else
     puts "The line is currently empty."
   end

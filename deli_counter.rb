fba963e04047d39b42161a5aa3bde1f3bee77aa2
Adam Weissman <adam@comedytragedyepic.com>
2019-09-14 10:16:52 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -22,7 +22,7 @@ end
 def now_serving(deli)
   counter = deli.length
   if counter == 0
-    return "There is nobody waiting to be served!"
+    puts "There is nobody waiting to be served!"
   elsif counter > 0
     puts "Currently serving #{deli.first}."
     deli.shift

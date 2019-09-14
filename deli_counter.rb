367623faa458a62730ebcd83ea390200e5066391
Adam Weissman <adam@comedytragedyepic.com>
2019-09-14 09:42:42 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -4,7 +4,7 @@ def line(deli)
   if deli.length > 0
     the_line = deli.each_with_index {|name, index| ' #{index}. #{name}'}
     the_line.unshift("The line is currently:")
-    return the_line.join
+    return the_line.join.to_s
   else
     puts "The line is currently empty."
   end

5a101b66efbb36d3e4cf5276a75fddbea7412e92
Adam Weissman <adam@comedytragedyepic.com>
2019-09-14 09:32:24 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -3,8 +3,10 @@
 def line(deli)
   if deli.length > 0
     puts "The line is currently: 
-    #{deli.each_with_index {|name, index| 
-      puts "#{name}. #{index} "} }
+    #{
+    deli.each_with_index {|name, index| 
+      puts "#{name}. #{index} "}
+      }
   else
     puts "The line is currently empty."
   end
